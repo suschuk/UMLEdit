d9f86b7dff09a217b1527ce4e1b4c4cc95fa8859
suschuk <suschuk@gmail.com>
2013-07-23 19:10:44 +0300

    - simplest windows application / MessageBox only

---
+++ UMLEdit/main.cpp
@@ -0,0 +1,9 @@
+#include "windows.h"
+
+
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, L"hi", L"Attention", MB_OK);
+    return 0;
+}
